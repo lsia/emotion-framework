8d682c38d2082bec42f5bc3f7df206fd74ce8e2b
Damian <damianmanoff@gmail.com>
2015-09-27 22:42:50 -0300

    Firefox extension update. - Refactor files - Panel view sample

--- FireFox Extension/index.js
+++ FireFox Extension/index.js
@@ -1,27 +1,48 @@
-var self = require('sdk/self');
+var data = require("sdk/self").data;
+// Construct a panel, loading its content from the "text-entry.html"
+// file in the "data" directory, and loading the "get-text.js" script
+// into it.
+var text_entry = require("sdk/panel").Panel({
+	width: 300,
+	height: 600,
+  	position: {
+   		top: 0,
+   		right: 0
+  	},
+  	contentURL: data.url("views/panel.view.html"),
+  	contentScriptFile: data.url("controllers/panel.controller.js")
+});
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-//function dummy(text, callback) {
-//  callback(text);
-//}
-
-//exports.dummy = dummy;
-
-var buttons = require('sdk/ui/button/action');
-var tabs = require("sdk/tabs");
-
-var button = buttons.ActionButton({
-  id: "Scrapper-link",
-  label: "Facebook Scrapper",
+// Create a button
+require("sdk/ui/button/action").ActionButton({
+  id: "show-panel",
+  label: "Show Panel",
   icon: {
-    "16": "./icon.png",
-    "32": "./icon.png",
-    "64": "./icon.png"
+    "16": "./imgs/icon.png",
+    "32": "./imgs/icon.png",
+    "64": "./imgs/icon.png"
   },
   onClick: handleClick
 });
 
+// Show the panel when the user clicks the button.
 function handleClick(state) {
-  tabs.open("http://www.facebook.com/");
+  text_entry.show();
 }
+
+// When the panel is displayed it generated an event called
+// "show": we will listen for that event and when it happens,
+// send our own "show" event to the panel's script, so the
+// script can prepare the panel for display.
+text_entry.on("show", function() {
+  text_entry.port.emit("show");
+});
+
+// Listen for messages called "text-entered" coming from
+// the content script. The message payload is the text the user
+// entered.
+// In this implementation we'll just log the text to the console.
+text_entry.port.on("text-entered", function (text) {
+  console.log(text);
+  text_entry.hide();
+});
